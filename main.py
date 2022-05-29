cfd59e504f845456b5200a8a3b92a41b3e9a31a3
Maxim <99118327+Maximz2@users.noreply.github.com>
2022-05-29 20:59:59 +0300

    Add "while True"

--- main.py
+++ main.py
@@ -14,21 +14,26 @@ if __name__ == '__main__':
     shop.add('коробки', 1)
     shop.add('лампочки', 2)
 
-    print(f"На складе есть \n {store.get_items()}")
-    print(f"В магазине есть \n {shop.get_items()}")
+    while True:
+        print(f"На складе есть \n {store.get_items()}")
+        print(f"В магазине есть \n {shop.get_items()}")
 
-    user_answer = input('Введите строку типа: "Доставить 3 собачки из склад в магазин": \n')
-    data_request = Request(user_answer)
+        user_answer = input('Введите строку типа: "Доставить 3 собачки из склад в магазин": \n')
+        data_request = Request(user_answer)
 
-    result_store = store.remove(data_request.product, data_request.amount)
-    if result_store:
-        print(f'Курьер забрал {data_request.amount} {data_request.product} со склада')
-        print(f'Курьер везет {data_request.amount} {data_request.product} со склада в магазин')
-        result_shop = shop.add(data_request.product, data_request.amount)
-        if not result_shop:
-            store.add(data_request.product, data_request.amount)
-        else:
-            print(f'Курьер доставил {data_request.amount} {data_request.product} со склада в магазин')
+        result_store = store.remove(data_request.product, data_request.amount)
+        if result_store:
+            print(f'Курьер забрал {data_request.amount} {data_request.product} со склада')
+            print(f'Курьер везет {data_request.amount} {data_request.product} со склада в магазин')
+            result_shop = shop.add(data_request.product, data_request.amount)
+            if not result_shop:
+                store.add(data_request.product, data_request.amount)
+            else:
+                print(f'Курьер доставил {data_request.amount} {data_request.product} со склада в магазин')
 
-    print(f'На складе есть: \n {store.get_items()}')
-    print(f'В магазине есть: \n {shop.get_items()}')
+        print(f'На складе есть: \n {store.get_items()}')
+        print(f'В магазине есть: \n {shop.get_items()}')
+        user_answer = input('Продолжаем?, Y/N: ')
+        if user_answer == "N":
+            break
+
